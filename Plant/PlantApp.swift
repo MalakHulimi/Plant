7cd442103473cfea173b4ecdd6e7d41026faef91
MalakHulimi <malakhulimi@gmail.com>
2023-01-08 15:07:16 +0300

    Initial Commit

---
+++ Plant/PlantApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantApp.swift
+//  Plant
+//
+//  Created by Malak  on 08/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
